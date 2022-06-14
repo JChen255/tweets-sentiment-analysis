97d0e786c8d656f092c3d862bba8183c5dece63c
Jialin Chen <98052549+JChen255@users.noreply.github.com>
2022-06-14 13:19:12 -0400

    Update predicting.py

--- predicting.py
+++ predicting.py
@@ -1,24 +1,25 @@
 import pickle
 import pandas as pd
+import argparse
 from preprocess import preprocess
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
 from sklearn.metrics import confusion_matrix
 
 
-def load_models():
+def load_models(vectorizer,BNB,LR):
 
     # Load the vectoriser.
-    file = open('140_vectoriser-ngram.pickle', 'rb')
+    file = open(vectorizer, 'rb')
     vectoriser = pickle.load(file)
     file.close()
 
     # Load the BNB Model.
-    file = open('140_BNB.pickle', 'rb')
+    file = open(BNB, 'rb')
     BNBmodel = pickle.load(file)
     file.close()
 
     # Load the LR Model.
-    file = open('140_LR.pickle', 'rb')
+    file = open(LR, 'rb')
     LRmodel = pickle.load(file)
     file.close()
 
@@ -42,37 +43,53 @@ def predict(vectoriser, model, text):
 
 
 if __name__ == "__main__":
-    # Loading the models.
-    vectoriser, LRmodel, BNBmodel = load_models()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--vectoriser_ngram", type=str, default="apple_vectoriser-ngram.pickle",
+                        help = "download the models from pickle files.")
+    parser.add_argument("--BNB_Model", type=str, default="apple_BNB.pickle",
+                        help = "download the models from pickle files.")
+    parser.add_argument("--LR_Model", type=str, default="apple_LR.pickle",
+                        help = "download the models from pickle files.")
+    parser.add_argument("--putin_test_dataset", type=str, default="Putin tweets.csv",
+                        help = "download the testing dataset.")
+    args = parser.parse_args()
+
+    # Loading the models from pickle files
+    vectorizer = args.vectoriser_ngram
+    BNB = args.BNB_Model
+    LR = args.LR_Model
+    vectoriser, LRmodel, BNBmodel = load_models(vectorizer, BNB, LR)
 
     # Text to classify should be in a list.
-    file = pd.read_csv("putin_test_dataset.csv")
+    file = pd.read_csv(args.putin_test_dataset)
     text = file["Text"]
     labels = file["label"]
 
-
+    # print out predictions for Logistic Regression Model
     pred1 = predict(vectoriser, LRmodel, text)[1]
     print(pred1)
+    # print out predictions for Logistic Regression Model
     pred2 = predict(vectoriser, BNBmodel, text)[1]
     print(pred2)
 
+    # Calculate specificity for Logistic Regression Model
     tn1, fp1, fn1, tp1 = confusion_matrix(labels, pred1).ravel()
     specificity1 = tn1 / (tn1 + fp1)
-
     print("specificity1",specificity1)
 
+    # Calculate specificity for Naive Bayes Model
     tn2, fp2, fn2, tp2 = confusion_matrix(labels, pred2).ravel()
     specificity2 = tn2 / (tn2 + fp2)
-
     print("specificity2", specificity2)
 
-
+    # Calculate metrics for Logistic Regression Model
     p1 = precision_score(labels, pred1)
     r1 = recall_score(labels, pred1)
     a1 = accuracy_score(labels, pred1)
     f1 = f1_score(labels, pred1)
     print(f"Sklearn scores for Logistic Regression Model: precision {p1:0.03}\trecall {r1:0.03}\taccuracy{a1:0.03}\tf1{f1:0.03}")
 
+    # Calculate metrics for Naive Bayes Model
     p2 = precision_score(labels, pred2)
     r2 = recall_score(labels, pred2)
     a2 = accuracy_score(labels, pred2)
